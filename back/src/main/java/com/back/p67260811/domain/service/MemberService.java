59277bd4b1e0e09f4a2f77e8aa9cea4b37e4b0cf
seul43 <l22seul43@gmail.com>
2026-09-01 00:52:17 +0000

    Member 관련 기능 추가

--- back/src/main/java/com/back/p67260811/domain/service/MemberService.java
+++ back/src/main/java/com/back/p67260811/domain/service/MemberService.java
@@ -1,4 +1,22 @@
 package com.back.p67260811.domain.service;
 
+import com.back.p67260811.domain.member.Member;
+import com.back.p67260811.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public long count() {
+        return memberRepository.count();
+    }
+
+    public Member join(String username, String password, String nickname) {
+        Member member = new Member(username, password, nickname);
+        return memberRepository.save(member);
+    }
 }
