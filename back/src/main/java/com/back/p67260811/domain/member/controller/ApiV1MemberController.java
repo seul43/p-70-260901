85d41ced55f9bad04a0ba1be9e781d04c4fc84d0
seul43 <l22seul43@gmail.com>
2026-09-02 09:34:46 +0900

    fix:동일 username 중복처리 MemberService로 이동

--- back/src/main/java/com/back/p67260811/domain/member/controller/ApiV1MemberController.java
+++ back/src/main/java/com/back/p67260811/domain/member/controller/ApiV1MemberController.java
@@ -46,10 +46,6 @@ public class ApiV1MemberController {
         @RequestBody @Valid JoinReqBody reqBody
     ) {
 
-        memberService.findByUsername(reqBody.username).ifPresent(m -> {
-            throw new ServiceException("409-1", "이미 사용중인 아이디입니다.");
-        });
-
         Member member = memberService.join(reqBody.username, reqBody.password, reqBody.nickname);
 
         return new RsData(
