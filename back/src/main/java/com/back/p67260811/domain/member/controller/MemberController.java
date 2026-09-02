bbb4cf261cd677b83e5bb916d7260a7c4f97b4fc
seul43 <l22seul43@gmail.com>
2026-09-02 09:11:40 +0900

    FEAT: 새유저

--- back/src/main/java/com/back/p67260811/domain/member/controller/MemberController.java
+++
@@ -1,5 +0,0 @@
-package com.back.p67260811.domain.member.controller;
-
-public class MemberController {
-
-}
---
+++ back/src/main/java/com/back/p67260811/domain/member/dto/MemberDto.java
@@ -0,0 +1,20 @@
+package com.back.p67260811.domain.member.dto;
+
+import com.back.p67260811.domain.member.Member;
+import java.time.LocalDateTime;
+
+public record MemberDto(
+    int id,
+    LocalDateTime createDate,
+    LocalDateTime modifyDate,
+    String name
+) {
+    public MemberDto(Member member) {
+        this(
+            member.getId(),
+            member.getCreateDate(),
+            member.getModifyDate(),
+            member.getNickname()
+        );
+    }
+}
